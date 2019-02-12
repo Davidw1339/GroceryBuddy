2fbd3a2e5ea8f4d30d1a8afdc91a113313c57583
David Wang <davidw1339@gmail.com>
2019-02-12 17:19:19 -0600

    Setting up TravisCI (#18)
    * added a juicy test :taco:
    
    * adding travis
    
    * frontend test
    
    * updating snapshot now
    
    * using fake timers now due to https://stackoverflow.com/questions/50793885/referenceerror-you-are-trying-to-import-a-file-after-the-jest-environment-has
    
    * added travis build to readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# GroceryBuddy
+# GroceryBuddy [![Build Status](https://travis-ci.com/Davidw1339/GroceryBuddy.svg?branch=master)](https://travis-ci.com/Davidw1339/GroceryBuddy)
 Software Engineering II Spring 2019 project
 clewell2
--- frontend/__tests__/Render-test.js
+++ frontend/__tests__/Render-test.js
@@ -5,6 +5,7 @@ import App from '../App';
 import renderer from 'react-test-renderer';
 
 test('renders correctly', () => {
+    jest.useFakeTimers();
     const tree = renderer.create(<App />).toJSON();
     expect(tree).toMatchSnapshot();
 });
