82144d4bbb81ecdcfdad8cdcc43a055df5570670
Zach Clewell <zclewell@gmail.com>
2019-02-18 18:26:38 -0600

    Zc/iteration 3 backend (#22)
    * initial version of mongo engine schema
    
    * initial version of mongo engine schema
    
    * removed version number from requirements
    
    * all lowercase
    
    * removed version number again
    
    * Removed mongoengine
    
    * Added back mongoengine
    
    * Added back version number
    
    * Removed mongoengine
    
    * Fixed compile issue with model.py

--- backend/model.py
+++ backend/model.py
@@ -1,22 +1,20 @@
 from flask_mongoengine import MongoEngine
 
 db = MongoEngine()
-
-class Price(db.EmbeddedDocument()):
+class Price(db.EmbeddedDocument):
     user = db.StringField()
     upvote = db.IntField()
     downvote = db.IntField()
     price = db.FloatField()
     date = db.DateTimeField()
 
-
-class Store(db.EmbeddedDocument()):
+class Store(db.EmbeddedDocument):
     name = db.StringField()
     location = db.DictField()
-    price = db.EmbeddedDocumentListField(db.EmbeddedDocumentField(Price))
+    price = db.EmbeddedDocumentListField(Price)
 
-class Item(db.Document()):
+class Item(db.Document):
     upc = db.StringField()
     name = db.StringField()
-    stores = db.EmbeddedDocumentListField(db.EmbeddedDocumentField(Store))
+    stores = db.EmbeddedDocumentListField(Store)
 
