8b0a0e82d02c20894517e1830eda2c82ca2bc29d
mongy910 <matthew910@gmail.com>
2019-03-07 21:36:00 -0600

    added dummy data file (#53)

---
+++ frontend/utils/dummyData.js
@@ -0,0 +1,278 @@
+export const shoppingDummyList = [
+  {
+    name: 'Veggies and Snacks',
+    store: 'Schnucks',
+    items: [
+      {
+        name: 'Strawberries',
+        price: 4.50,
+        upvote: 1,
+        downvote: 1,
+        image_url: 'https://i.imgur.com/Z8WLg.jpg',
+        quantity: 1,
+        upc: '002986005505',
+      },
+      {
+        name: 'Bananas',
+        price: 1.49,
+        upvote: 4,
+        downvote: 0,
+        image_url: 'https://i.imgur.com/V8SS4Yk.jpg',
+        quantity: 2,
+        upc: '506023777057',
+      },
+      {
+        name: 'Cookies',
+        price: 2.55,
+        upvote: 2,
+        downvote: 0,
+        image_url: 'https://i.imgur.com/I1SyBTh.jpg',
+        quantity: 1,
+        upc: '743105000017',
+      },
+      {
+        name: 'Baked Lays',
+        price: 1.99,
+        upvote: 1,
+        downvote: 0,
+        image_url: 'https://i.imgur.com/16mmVAM.jpg',
+        quantity: 1,
+        upc: '743105000017',
+      },
+    ]
+  }
+]
+
+export const createListDummyItems = [
+  {
+    name: 'Apples',
+    image_url:
+      'https://i.imgur.com/fSyVUBb.jpg',
+    upc: '885909373178',
+    stores: [
+      {
+        name: 'County Market',
+        location: {
+          lat: 50.25,
+          long: 20.11
+        },
+        prices: [
+          {
+            user: 'test_user',
+            upvote: 2,
+            downvote: 1,
+            price: 1.99,
+            date: 1552004035
+          },
+          {
+            user: 'test_user2',
+            upvote: 1,
+            downvote: 0,
+            price: 2.00,
+            date: 1552004024
+          }
+        ]
+      },
+      {
+        name: 'Schnucks',
+        location: {
+          lat: 51.25,
+          long: 25.11
+        },
+        prices: [
+          {
+            user: 'test_user',
+            upvote: 2,
+            downvote: 1,
+            price: 2.99,
+            date: 1552004035
+          },
+          {
+            user: 'test_user2',
+            upvote: 1,
+            downvote: 0,
+            price: 3.99,
+            date: 1552004024
+          }
+        ]
+      },
+      {
+        name: 'Walgreens',
+        location: {
+          lat: 61.25,
+          long: 25.11
+        },
+        prices: [
+          {
+            user: 'test_user',
+            upvote: 2,
+            downvote: 1,
+            price: 4.99,
+            date: 1552004035
+          },
+          {
+            user: 'test_user2',
+            upvote: 1,
+            downvote: 0,
+            price: 5.99,
+            date: 1552004024
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: 'Bananas',
+    image_url:
+      'https://i.imgur.com/V8SS4Yk.jpg',
+    upc: '506023777057',
+    stores: [
+      {
+        name: 'County Market',
+        location: {
+          lat: 50.25,
+          long: 20.11
+        },
+        prices: [
+          {
+            user: 'test_user',
+            upvote: 2,
+            downvote: 1,
+            price: 7.99,
+            date: 1552004035
+          },
+          {
+            user: 'test_user2',
+            upvote: 1,
+            downvote: 0,
+            price: 3.00,
+            date: 1552004024
+          }
+        ]
+      },
+      {
+        name: 'Schnucks',
+        location: {
+          lat: 51.25,
+          long: 25.11
+        },
+        prices: [
+          {
+            user: 'test_user',
+            upvote: 2,
+            downvote: 1,
+            price: 2.99,
+            date: 1552004035
+          },
+          {
+            user: 'test_user2',
+            upvote: 1,
+            downvote: 0,
+            price: 3.99,
+            date: 1552004024
+          }
+        ]
+      },
+      {
+        name: 'Walgreens',
+        location: {
+          lat: 61.25,
+          long: 25.11
+        },
+        prices: [
+          {
+            user: 'test_user',
+            upvote: 2,
+            downvote: 1,
+            price: 4.99,
+            date: 1552004035
+          },
+          {
+            user: 'test_user2',
+            upvote: 1,
+            downvote: 0,
+            price: 5.99,
+            date: 1552004024
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: 'Cookies',
+    image_url:
+      'https://i.imgur.com/I1SyBTh.jpg',
+    upc: '743105000017',
+    stores: [
+      {
+        name: 'County Market',
+        location: {
+          lat: 50.25,
+          long: 20.11
+        },
+        prices: [
+          {
+            user: 'test_user',
+            upvote: 2,
+            downvote: 1,
+            price: 7.99,
+            date: 1552004035
+          },
+          {
+            user: 'test_user2',
+            upvote: 1,
+            downvote: 0,
+            price: 3.00,
+            date: 1552004024
+          }
+        ]
+      },
+      {
+        name: 'Schnucks',
+        location: {
+          lat: 51.25,
+          long: 25.11
+        },
+        prices: [
+          {
+            user: 'test_user',
+            upvote: 2,
+            downvote: 1,
+            price: 2.99,
+            date: 1552004035
+          },
+          {
+            user: 'test_user2',
+            upvote: 1,
+            downvote: 0,
+            price: 3.99,
+            date: 1552004024
+          }
+        ]
+      },
+      {
+        name: 'Walgreens',
+        location: {
+          lat: 61.25,
+          long: 25.11
+        },
+        prices: [
+          {
+            user: 'test_user',
+            upvote: 2,
+            downvote: 1,
+            price: 4.99,
+            date: 1552004035
+          },
+          {
+            user: 'test_user2',
+            upvote: 1,
+            downvote: 0,
+            price: 5.99,
+            date: 1552004024
+          }
+        ]
+      }
+    ]
+  },
+];
