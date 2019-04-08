147a88f3a55644e1d233c78508c25065d3d86c6e
Ray Sun <rsun0@users.noreply.github.com>
2019-04-08 14:07:47 -0500

    Set model fields to required (#121)

--- backend/model.py
+++ backend/model.py
@@ -4,24 +4,24 @@ import validation
 
 
 class Price(EmbeddedDocument):
-    user = StringField(min_length=1, max_length=64)
+    user = StringField(min_length=1, max_length=64, required=True)
     upvotes = ListField(StringField(min_length=1, max_length=64))
     downvotes = ListField(StringField(min_length=1, max_length=64))
-    price = DecimalField(min_value=0, precision=2)
-    date = DecimalField(min_value=0, precision=6)
+    price = DecimalField(min_value=0, precision=2, required=True)
+    date = DecimalField(min_value=0, precision=6, required=True)
 
 
 class Store(EmbeddedDocument):
-    name = StringField(min_length=1, max_length=64)
-    location = DictField()
+    name = StringField(min_length=1, max_length=64, required=True)
+    location = DictField(required=True)
     location.validate = validation.validate_location
-    prices = EmbeddedDocumentListField(Price)
+    prices = EmbeddedDocumentListField(Price, required=True)
 
 
 class Item(Document):
     meta = {'collection': 'grocery-items'}
-    upc = StringField(max_length=12, min_length=12)
+    upc = StringField(max_length=12, min_length=12, required=True)
     upc.validate = validation.validate_upc
-    name = StringField(min_length=1, max_length=64)
+    name = StringField(min_length=1, max_length=64, required=True)
     image_url = StringField(min_length=0, max_length=512)
-    stores = EmbeddedDocumentListField(Store)
+    stores = EmbeddedDocumentListField(Store, required=True)
